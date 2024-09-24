cb7b58f056ddca2cccac9c9c04cd3e858a4c0e4e
12JS0927 <12JS0927@KB-PF3AMRTA>
2024-09-24 11:04:36 +0200

    good good I guess

--- Roulett/Program.cs
+++ Roulett/Program.cs
@@ -6,21 +6,24 @@ number = Convert.ToInt32(input);
 
 
 int number2;
-Console.WriteLine("Please pick a number between 1-30");
+string text = "Red";
+string text2 = "Black";
+string text3 = "Green";
+Console.WriteLine("Please pick a number between 1-30 or a color between Red, Black or Green");
 string input2 = Console.ReadLine();
 number2 = Convert.ToInt32(input2);
 
 
 Random rand = new Random();
-Console.WriteLine(rand.Next());
 
 int number3;
 number3 = rand.Next(1, 30);
 Console.WriteLine(number3);
 
 
+
 if(number3 == number2) {
-    Console.WriteLine("yay you won!!!!");
+    Console.WriteLine("yay you won!!!! ^u^");
 } else {
     Console.WriteLine("better luck next time :(  T_T");
 }
